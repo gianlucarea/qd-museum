f5c2187b1dbe71b26c074e99de0b9d54275b2443
Capantonio <fabiocap95@live.it>
2022-02-10 22:42:38 +0100

    Initial work on Tracking Functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Input\Input;
+use Auth;
 
 class UserController extends Controller
 {
+    /**
+     * Get all information about user
+     * 
+     * @return \Illuminate\View\Profile
+     */
+    public function getUserInfo(Request $request)
+    {   
+        $user = Auth::user();
+        return view('userProfile', $user);
+    }
 
+    public function getWorkingMuseum(Request $request)
+    {
+        $user = Auth::user();
+    }
+
+    /**
+     * Track the user on the visit, async update
+     * 
+     * @return \Illuminate\View\Visit
+     */
+    public function userTracking(Request $request)
+    {
+        $user = Auth::user();
+    }
+
+    /**
+     * Operator controll for visitor management on the museum
+     * 
+     * @return \Illuminate\View\VisitorControl
+     */
+    public function operatorTracking (Request $request)
+    {
+        $user = Auth::user();
+    }
 
 }
